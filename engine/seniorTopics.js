b119918057617a78d8189e295d3bb90e14aebc84
yutveg <kodi.0n31l@gmail.com>
2020-07-30 14:23:57 -0700

    hoisting, lexical scope

--- engine/seniorTopics.js
+++ engine/seniorTopics.js
@@ -1,6 +1,9 @@
-// Execution Context
+/* 
+||  Execution Context  ||
+*/
 // Engine creates a global() execution context initially
 // Comes with Global Object, and this keyword
+// Global variables are added onto the global object
 function printName() {
   return "Andrew Neagoie";
 }
@@ -19,3 +22,28 @@ function sayMyName() {
 sayMyName();
 
 // global() execution context popped off after program done
+
+/*
+||  Lexical Scope/Environment  ||
+*/
+// relating to the location of program instructions are defined in a file
+// dynamic scope relates to location when invoked/called
+
+/*
+|| Hoisting ||
+*/
+// first pass, allocates memory for vars and functions before running
+sing();
+console.log(teddy);
+// let and const are not hoisted
+// var partially hoisted
+const teddy = "bear";
+// function declarations fully hoisted, last declaration
+// takes priority
+function sing() {
+  console.log("ohhhhh lalala");
+}
+// function expressions partially / not hoisted
+var sing2 = function () {
+  console.log("uhhh lalala");
+};
