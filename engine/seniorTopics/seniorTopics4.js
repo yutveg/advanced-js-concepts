1853615e32d94b0ab21e23276346626b175fc7ae
yutveg <kodi.0n31l@gmail.com>
2020-08-03 12:04:30 -0700

    currying functions with binding

--- engine/seniorTopics/seniorTopics4.js
+++ engine/seniorTopics/seniorTopics4.js
@@ -5,3 +5,9 @@ function multiply(a, b) {
 
 let multiplyByTwo = multiply.bind(this, 2);
 console.log(multiplyByTwo(3));
+
+// Context vs Scope
+// scope is more related to the variable access of a function
+// when it is invoked
+
+// context is more object, "this" related
