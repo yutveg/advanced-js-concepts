b31f12cb72f3489ea285349febcf47e2e60d3817
yutveg <kodi.0n31l@gmail.com>
2020-08-03 11:54:46 -0700

    this keyword in depth, as well as call, apply, bind

--- engine/seniorTopics/seniorTopics3.js
+++ engine/seniorTopics/seniorTopics3.js
@@ -59,6 +59,7 @@ const obj = {
   sing() {
     // this points at object
     console.log("a", this);
+    // regular functions are dynamically bound
     // arrow functions are LEXICALLY bound
     // this = parent object
     var anotherFunc = function () {
@@ -70,3 +71,26 @@ const obj = {
 };
 
 obj.sing();
+
+// call(); method on function objects to invoke
+// apply(); method on function objects to invoke, takes parameters as array after
+// specifying context
+// bind(); RETURNS a function, creating a context + takes parameters
+
+const wizard = {
+  name: "Merlin",
+  health: 100,
+  heal(num1, num2) {
+    return (this.health += num1 + num2);
+  },
+};
+
+const archer = {
+  name: "Robin Hood",
+  health: 30,
+};
+
+wizard.heal.call(archer, 50, 100); // calling the heal method, using "archer" as the 'this'
+console.log(archer);
+wizard.heal.apply(archer, [20, 30]);
+console.log(archer);
