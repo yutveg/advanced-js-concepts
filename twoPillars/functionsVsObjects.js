c54fb38d0c68445653120b0586674586f8a1a51b
yutveg <kodi.0n31l@gmail.com>
2020-08-04 16:20:45 -0700

    why functions are first class citizens

--- twoPillars/functionsVsObjects.js
+++ twoPillars/functionsVsObjects.js
@@ -16,3 +16,25 @@ four.someProperty = "ahhh";
 //     name: 'four', // an optional attribute
 //     (): return num
 // }
+
+// Functions are first class citizens in JS
+// - can be assigned to variables, and properties of objs
+var stuff = function () {};
+// - can pass functions as arguments
+function a(fn) {
+  fn();
+}
+
+a(function () {
+  console.log("hi there");
+});
+
+// - can return functions as values
+
+function b() {
+  return function someFunction() {
+    console.log("hi");
+  };
+}
+
+console.log(b()());
