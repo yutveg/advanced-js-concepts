eceeb3f52cf7c13878e7c477ec6fbd6a92a5f358
yutveg <kodi.0n31l@gmail.com>
2020-08-06 15:34:08 -0700

    closures three iife example

---
+++ twoPillars/closures/closures3.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4];
+for (let i = 0; i < array.length; i++) {
+  setTimeout(function () {
+    console.log("I am at index", array[i]);
+  }, 3000);
+}
+
+for (var i = 0; i < array.length; i++) {
+  (function (closureI) {
+    setTimeout(function () {
+      console.log("I am at index", array[closureI]);
+    }, 3000);
+  })(i);
+}
