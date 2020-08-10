c970732e34bbb6996f4e68fa6e06bcf77a5b8d84
yutveg <kodi.0n31l@gmail.com>
2020-08-10 16:26:10 -0700

    functions have prototypes, proto points to prototype, object prototype is the actual object

--- twoPillars/prototypes/inheritance2.js
+++ twoPillars/prototypes/inheritance2.js
@@ -21,7 +21,7 @@ let lizard = {
 // setting the lizard's prototype template to inherit from the dragon object
 // v-- should never use this syntax to manually create chains, inefficient
 lizard.__proto__ = dragon;
-// object -> dragon -> lizard
+// null -> object -> dragon -> lizard
 //         ^--       ^-- each arrow indicates a prototype
 for (let prop in lizard) {
   // searches up the prototype chain for a property
--- twoPillars/prototypes/inheritance3.js
+++ twoPillars/prototypes/inheritance3.js
@@ -2,4 +2,21 @@ function a() {}
 console.log(a.hasOwnProperty("call"));
 // returns false, functions do not have bind, call, or apply
 // on themselves, it comes from the base function constructor
-// object -> function constructor -> function a
+// null -> object -> function constructor -> function a
+
+// prototype {
+//     ...props,
+//     __proto__: someProtoype
+// }
+// __proto__ is simply a pointer to some other prototype
+
+let human = {
+  mortal: true,
+};
+// better method to use than directly setting __proto__
+let socrates = Object.create(human);
+console.log(socrates.mortal);
+
+// Object is a function, because it has the prototype property
+// typeof Object === function
+// Object.prototype is the base object
