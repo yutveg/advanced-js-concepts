46582ceea848237621fd63fb3c9d9f2d21b4cca5
yutveg <kodi.0n31l@gmail.com>
2020-08-20 12:10:11 -0700

    currying

--- webDev/functions.js
+++ webDev/functions.js
@@ -8,3 +8,13 @@ const first = () => {
 
 const newFunc = first();
 console.log(newFunc());
+
+// currying
+const multiply = (a, b) => a * b;
+
+const curriedMultiply = (a) => (b) => a * b;
+// process of turning a single, multi parameter function
+// into a series of functions that take one parameter each
+const multiplyByThree = curriedMultiply(3);
+// an example use case:
+console.log(multiplyByThree(20));
