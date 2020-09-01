6118f354be1e1fe71f504d90a443c23d54e29cc8
yutveg <kodi.0n31l@gmail.com>
2020-09-01 13:59:01 -0700

    queue from two stacks complete

--- interviewCake/dataStructures/stacks/stackAndMaxStack.js
+++ interviewCake/dataStructures/stacks/stackAndMaxStack.js
@@ -1,32 +1,4 @@
-class Stack {
-  constructor() {
-    // Initialize an empty stack
-    this.items = [];
-  }
-
-  // Push a new item onto the stack
-  push(item) {
-    this.items.push(item);
-  }
-
-  // Remove and return the last item
-  pop() {
-    // If the stack is empty, return null
-    // (It would also be reasonable to throw an exception)
-    if (!this.items.length) {
-      return null;
-    }
-    return this.items.pop();
-  }
-
-  // Returns the last item without removing it
-  peek() {
-    if (!this.items.length) {
-      return null;
-    }
-    return this.items[this.items.length - 1];
-  }
-}
+const Stack = require("./Stack");
 
 class MaxStack {
   constructor() {
